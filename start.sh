df449a360f7cfabaae29896a4733d6bc2e95d512
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-28 21:39:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,6 +23,6 @@
 #!/bin/bash
 
 set -e
-export FLASK_APP=anu:create_app
-gunicorn -w 4 -b 0.0.0.0:${PORT:-8080} anu:create_app &
-python3 STORM.py
+export FLASK_APP=check:create_app
+gunicorn -w 4 -b 0.0.0.0:${PORT:-8080} check:create_app &
+python3 FLASH.py
