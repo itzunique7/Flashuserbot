4dd140403d0812387b4369bf9e3880a5715bacd0
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-29 19:49:11 +0530

    Create dino.py

---
+++ FLASH/modules/dino.py
@@ -0,0 +1,87 @@
+#MIT License
+
+#Copyright (c) 2024 DHIRAJ [AFK]
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import SUDO_USERS
+import asyncio
+hl = "."
+
+
+@Client.on_message(
+    filters.command(["dino"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def adadino(client: Client, message: Message):
+    typew = await edit_or_reply(message, "`DIN DINNN.....`")
+    await asyncio.sleep(1)
+    await typew.edit("`DINOOOOSAURUSSSSS!!`")
+    await asyncio.sleep(1)
+    await typew.edit("`🏃                        🦖`")
+    await typew.edit("`🏃                       🦖`")
+    await typew.edit("`🏃                      🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃   `Larius`         🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃WORGH!   🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                    🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃  Huh-Huh           🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃          🦖`")
+    await typew.edit("`🏃         🦖`")
+    await typew.edit("`HE'S GETTING CLOSER!!!`")
+    await asyncio.sleep(1)
+    await typew.edit("`🏃       🦖`")
+    await typew.edit("`🏃      🦖`")
+    await typew.edit("`🏃     🦖`")
+    await typew.edit("`🏃    🦖`")
+    await typew.edit("`Just give up`")
+    await asyncio.sleep(1)
+    await typew.edit("`🧎🦖`")
+    await asyncio.sleep(2)
+    await typew.edit("`-ENDED-`")
+  
