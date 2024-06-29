4485bc4c428baa3690d7ca8ec9630e5f1f09f2c0
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-29 05:34:01 +0530

    Update dare.py

--- FLASH/modules/dare.py
+++ FLASH/modules/dare.py
@@ -26,7 +26,7 @@ from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from STORMDB.data import STORMS, DARE
+from FLASHDB.data import STORMS, DARE
 from config import OWNER_ID, SUDO_USERS
 
 @Client.on_message(
