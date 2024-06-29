8e00546cc4d712fd2889d13e8e32b44b475cff98
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-29 12:08:49 +0530

    Update shayari.py

--- FLASH/modules/shayari.py
+++ FLASH/modules/shayari.py
@@ -23,7 +23,7 @@
 from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from STORMDB.data import STORMS, SRAID
+from FLASHDB.data import STORMS, SRAID
 from config import SUDO_USERS, OWNER_ID
 import asyncio
 
