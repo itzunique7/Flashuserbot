6b9e950307b7f438a4484bbc69782181cd120bbe
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-30 05:57:22 +0530

    Update wtf.py

--- FLASH/modules/wtf.py
+++ FLASH/modules/wtf.py
@@ -19,7 +19,6 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
-from PIL import Image
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import SUDO_USERS
