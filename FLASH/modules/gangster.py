ace2ca5eaa18f5d714309a43e36ba56209c709eb
𝐃𝐇𝐈𝐑𝐀𝐉 <mauryadheeraj226@gmail.com>
2024-06-30 08:22:00 +0530

    Update gangster.py

--- FLASH/modules/gangster.py
+++ FLASH/modules/gangster.py
@@ -23,6 +23,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import SUDO_USERS
+from FLASH.helpers.basic import edit_or_reply, get_text
 import asyncio
 hl = "."
 
